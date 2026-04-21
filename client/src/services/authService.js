11d9eb56d6412d0f96a8794bc5a0c4992be23e9a
MohdAnas1234 <mohdanas2403@gmail.com>
2026-04-22 00:53:57 +0530

    2nd Fix

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api/auth',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/auth',
 })
 
-export const signup = (data) => API.post('/signup', data)
+export const signup = (data) => API.post('/auth/signup', data)
 
-export const login = (data) => API.post('/login', data)
+export const login = (data) => API.post('/auth/login', data)
